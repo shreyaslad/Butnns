fb731262f341e58f9a39302dd8f0f28773ce78a8
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-06 16:36:50 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-  $('body').load(function() {
-    $('body').fadein('3000');
-  });
-});
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 210) {
+                    $("body").css('background-color', 'blue');
+                } else {
+                    $("body").css('background-color', 'red');
+                }
+            });
+        });
